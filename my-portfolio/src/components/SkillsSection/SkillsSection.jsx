340518ff50d346447d261c1dad103a60251cc4dd
Nikki Lionel Ocer <nlocer97@gmail.com>
2024-09-23 19:19:44 +0800

    create Skills Section

--- my-portfolio/src/components/SkillsSection/SkillsSection.jsx
+++ my-portfolio/src/components/SkillsSection/SkillsSection.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import skillsData from "./SkillsData";
 
 function SkillsSection() {
+  console.log(skillsData);
   return (
-    <div>SkillsSection</div>
-  )
+    <div className="py-5">
+      <div className="container px-lg-5 px-sm-0">
+        <div className="d-flex">
+          <p className="section-title-style">Skills</p>
+        </div>
+        <div className="pt-5">
+          <p className="text-center">
+            The skills, tools, and technologies I've worked with:
+          </p>
+
+          <div className="row">
+            {skillsData.map((item) => {
+              return (
+                <div className="col-lg-2 col-md-3 col-sm-4 col-4 skill-logo d-flex flex-column align-items-center">
+                  {item.logo}
+                  <p>{item.name}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SkillsSection
+export default SkillsSection;
--- my-portfolio/src/pages/Home.jsx
+++ my-portfolio/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import NavigationBar from "../components/NavigationBar/NavigationBar";
 import HomeSection from "../components/HomeSection/HomeSection";
 import AboutMeSection from "../components/AboutMeSection/AboutMeSection";
+import SkillsSection from "../components/SkillsSection/SkillsSection";
 
 function Home() {
   return (
-    <div style={{height: "100%"}}>
+    <div style={{ height: "100%" }}>
       <NavigationBar />
       <HomeSection />
       <AboutMeSection />
+      <SkillsSection />
     </div>
   );
 }
